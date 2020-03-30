528e6d966a8c806cc9fa3b59404e1c7b1e9771b2
Necro73 <swordigo7@gmail.com>
2020-03-30 15:43:51 +0300

    1 file modified

--- index.js
+++ index.js
@@ -4,35 +4,35 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 var num = [];
-		var x;
-		var truer = [];
-		truer[0] = 1;
-		truer[1] = 2;
-		truer[2] = 3;
-		truer[3] = 4;
-		truer[4] = 5;
-		truer[5] = 6;
-		function sendForm(event)
-		{
-			// получаем значение поля кнопки
-			var idopj = event.target.id;
-			if(num[idopj] == truer[0])
-			{
-				alert("Успех!");
-			}
-			else
-			{
-				alert("Провал!");
-			}
-		}
+var x;
+var truer = [];
+truer[0] = 1;
+truer[1] = 2;
+truer[2] = 3;
+truer[3] = 4;
+truer[4] = 5;
+truer[5] = 6;
+function sendForm(event)
+{
+	// получаем значение поля кнопки
+	var idopj = event.target.id;
+	if(num[idopj] == truer[0])
+	{
+		alert("Успех!");
+	}
+	else
+	{
+		alert("Провал!");
+	}
+	}
  
-    //for (var i = 1; i < 49; i++)
-   // {
-   //   num[i-1] = i;
-  //  }
+//for (var i = 1; i < 49; i++)
+//{
+//  num[i-1] = i;
+//}
 
-    for (var i = 1; i < 49; i++)
-    {
-      var sendButton = document.getElementById("valuer_" + i);
-      sendButton.addEventListener("click", sendForm);
-    }
+for (var i = 1; i < 49; i++)
+{
+  var sendButton = document.getElementById("valuer_" + i);
+  sendButton.addEventListener("click", sendForm);
+}
